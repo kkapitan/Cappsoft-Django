e81484fbc9306f1a27a247a4b0dc2ed918bdf1ba
Krzysztof <krzysztof.kapitan@gmail.com>
2017-01-13 22:42:53 +0100

    Integrate django-registration-redux

--- cappsoftcms/settings.py
+++ cappsoftcms/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cms_core',
+    'registration',
 ]
+
+REGISTRATION_OPEN = True  # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
