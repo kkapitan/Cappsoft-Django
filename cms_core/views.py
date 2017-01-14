d1c2d3cac9cd3720e05291ea382039f287464450
Krzysztof <krzysztof.kapitan@gmail.com>
2017-01-14 16:58:06 +0100

    Display posts list

--- cms_core/views.py
+++ cms_core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-def index(request):
-    return render(request,'index.html')
+
+def post_list(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'index.html', {"posts": posts})
