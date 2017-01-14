4e0de31c6f6038a810f4497887adb89b0d91b937
Krzysztof <krzysztof.kapitan@gmail.com>
2017-01-14 15:11:32 +0100

    Add css

--- cappsoftcms/urls.py
+++ cappsoftcms/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from cms_core import views as cms_core
 
 urlpatterns = [
+    url(r'^$', cms_core.index, name="index"),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls'))
 ]
+
