bc2ee11b652cd38ba4bf480a67794fbe898b2871
Krzysztof <krzysztof.kapitan@gmail.com>
2017-01-16 13:28:29 +0100

    Add dropdown for navigation

--- cms_core/urls.py
+++ cms_core/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^users/', views.user_list, name='users_list'),
+    url(r'^users/', views.user_list, name='user_list'),
     url(r'^user/(?P<pk>[0-9]+)', views.user_detail, name='user_detail'),
 ]
